022e14fc96eb86b08d5a01ac00d71f25f94e86ea
lumpn <lumpn@users.noreply.github.com>
2024-04-09 14:20:37 +0800

    Moved classes to their own files

---
+++ Tuple.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using VValue = System.Collections.Generic.KeyValuePair<int, int>;
+
+public struct Tuple
+{
+    public VValue[] vs;
+
+    public void WriteTo(TextWriter writer)
+    {
+        writer.Write("(");
+        for (int i = 0; i < vs.Length; i++)
+        {
+            var v = vs[i];
+            v.WriteTo(writer);
+            if (i < vs.Length - 1)
+            {
+                writer.Write(", ");
+            }
+        }
+        writer.Write(")");
+    }
+}
---
+++ WriteExtentions.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System.Linq;
+using Domain = System.Collections.Generic.HashSet<System.Collections.Generic.KeyValuePair<int, int>>;
+using VValue = System.Collections.Generic.KeyValuePair<int, int>;
+
+public static class WriteExtentions
+{
+    public static void WriteTo(this VValue v, TextWriter writer)
+    {
+        writer.Write("(");
+        writer.Write(v.Key);
+        writer.Write(", ");
+        writer.Write(v.Value);
+        writer.Write(")");
+    }
+
+    public static void WriteTo(this Domain domain, TextWriter writer)
+    {
+        foreach (var v in domain.OrderBy(p => p.Key).ThenBy(p => p.Value))
+        {
+            v.WriteTo(writer);
+            writer.WriteLine();
+        }
+    }
+
+}
